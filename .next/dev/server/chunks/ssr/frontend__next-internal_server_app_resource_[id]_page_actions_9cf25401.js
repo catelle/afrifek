db4610da3d80873e1539f319a31434a6e41f4ef9
catelle <catelle@localhost.localdomain>
2025-12-13 23:59:12 +0100

    Update .gitignore and remove build files

--- .next/dev/server/chunks/ssr/frontend__next-internal_server_app_resource_[id]_page_actions_9cf25401.js
+++
@@ -1,7 +0,0 @@
-module.exports = [
-"[project]/frontend/.next-internal/server/app/resource/[id]/page/actions.js [app-rsc] (server actions loader, ecmascript)", ((__turbopack_context__, module, exports) => {
-
-}),
-];
-
-//# sourceMappingURL=frontend__next-internal_server_app_resource_%5Bid%5D_page_actions_9cf25401.js.map
--- .next/dev/static/chunks/frontend_src_app_resource_[id]_page_tsx_5f7fdfbd._.js
+++
@@ -1,8 +0,0 @@
-(globalThis.TURBOPACK_CHUNK_LISTS || (globalThis.TURBOPACK_CHUNK_LISTS = [])).push({
-    script: typeof document === "object" ? document.currentScript : undefined,
-    chunks: [
-  "static/chunks/frontend_src_e42e5918._.js",
-  "static/chunks/9e883_7d9173c2._.js"
-],
-    source: "dynamic"
-});
